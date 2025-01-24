a7cab3e0bd5705449a11d6707dab191562e49611
maurohenriques-arch <mauro.henriques@gmail.com>
2025-01-24 18:32:20 +0000

    New JWK methods

---
+++ TokenManagerTests/TokenManagerServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TokenManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TokenManager.Tests
+{
+    [TestClass()]
+    public class TokenManagerServiceTests
+    {
+        [TestMethod()]
+        public void DecodeTokenFromPrivateKeyJWTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EncodeTokenFromPrivateKeyJWTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePublicJWKForEncryptionForPrivateKeyJWTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePrivateJWKForEncryptionForPrivateKeyJWTTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateECJWKPairTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateRSAJWKPairTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PKJWT_GetBuildInfo_ExtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecodeTokenFromPrivateKeyJWTTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EncodeTokenFromPrivateKeyJWTTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePublicJWKForEncryptionForPrivateKeyJWTTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePrivateJWKForEncryptionForPrivateKeyJWTTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateECJWKPairTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateRSAJWKPairTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PKJWT_GetBuildInfo_ExtTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
