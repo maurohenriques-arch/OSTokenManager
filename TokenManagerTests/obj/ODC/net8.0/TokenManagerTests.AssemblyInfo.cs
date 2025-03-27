50f84db416fe4d0aaaa695b792f15a90e9243cd9
osseramend <sergio.amendoeira@outsystems.com>
2025-03-27 11:34:33 +0000

    Added Publich Configuration

--- TokenManagerTests/obj/ODC/net8.0/TokenManagerTests.AssemblyInfo.cs
+++ TokenManagerTests/obj/ODC/net8.0/TokenManagerTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TokenManagerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ODC")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56eb1402f8050034cbb6622ba2e160e18ffc4cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8450a0d36a291faf50a5b6d87200181df7472a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TokenManagerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TokenManagerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
