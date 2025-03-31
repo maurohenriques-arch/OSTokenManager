618efb20c246441ae747367fe531d92616f89e64
maurohenriques-arch <mauro.henriques@gmail.com>
2025-03-31 18:30:21 +0100

    New commad bat...optmized

--- Compress.bat
+++ Compress.bat
@@ -1,3 +1,8 @@
-del ".\Binaries\TokenManager.zip"
-powershell Compress-Archive -Path ".\TokenManager\bin\Release\net8.0\linux-x64\publish\*" -DestinationPath ".\Binaries\TokenManager"
-readline()
+@echo off
+ECHO Deleting old archive if it exists...
+DEL /Q ".\Binaries\TokenManager.zip"
+
+ECHO Creating new archive...
+powershell -Command "Compress-Archive -Path '.\TokenManager\bin\Release\net8.0\linux-x64\publish\*' -DestinationPath '.\Binaries\TokenManager.zip' -Force"
+
+ECHO Done.
